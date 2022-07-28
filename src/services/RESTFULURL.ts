a2a594e701f2a6ac7e2714bdf830efc346ddc6fa
leiweibo <406637644@qq.com>
2022-07-28 17:35:20 +0800

    update

--- src/services/RESTFULURL.ts
+++ src/services/RESTFULURL.ts
@@ -14,10 +14,6 @@ export default {
   joinCompanys: '/orginone/organization/company/get/joined/companys', // 获取组织列表
   cohort: {
     join: cohort + '/join' // 加群
-  },
-<<<<<<< HEAD
-  test: '/orginone/organization/cohort/get/authoritys'
-=======
->>>>>>> df6c735861fc5960a1ac8853dac0ffec0c50c125
+  }
   //others
 }
