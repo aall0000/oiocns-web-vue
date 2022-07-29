22fd72daf1f687181908ac46e639e0937591b8a1
SEN <changsen25@163.com>
2022-07-29 14:39:06 +0800

    feat:组织架构功能

---
+++ src/views/Group/OrganizationTree/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="organization-wrap">
+    <div class="organization-head">
+      <div class="organization-type">群 / 消息 / 组织架构</div>
+    </div>
+    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+      <el-tab-pane label="成员" name="first">成员</el-tab-pane>
+      <el-tab-pane label="单位" name="second">单位</el-tab-pane>
+      <el-tab-pane label="集团" name="third">集团</el-tab-pane>
+    </el-tabs>
+    <div class="content">
+      <div class="tab-list">
+        <el-table :data="tableData" stripe style="width: 100%" @select="select">
+          <el-table-column prop="date" type="selection" width="180" />
+          <el-table-column prop="date" label="Date" width="180" />
+          <el-table-column prop="name" label="Name" width="180" :filters="[
+            { text: '2016-05-01', value: '2016-05-01' },
+            { text: '2016-05-02', value: '2016-05-02' },
+            { text: '2016-05-03', value: '2016-05-03' },
+            { text: '2016-05-04', value: '2016-05-04' }
+          ]" />
+          <el-table-column prop="address" label="Address" :filter-method="filterHandler" />
+        </el-table>
+      </div>
+      <div class="page">
+        <el-pagination small background layout="prev, pager, next" :total="50" class="mt-4" />
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { TabsPaneContext } from 'element-plus'
+
+var tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  }
+]
+
+const activeName = ref('first')
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event)
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.organization-wrap {
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+}
+
+.organization-head {
+  padding: 20px;
+  background: #fff;
+
+  .organization-type {
+    font-size: 16px;
+    color: #8d8d8d;
+    margin-bottom: 15px;
+  }
+
+  .organization-mian {
+    font-size: 24px;
+    font-weight: bold;
+    color: #333;
+    display: flex;
+    justify-content: space-between;
+  }
+}
+
+.content {
+  background: #f0f2f5;
+  padding: 20px;
+
+  .search {
+    background: #fff;
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+
+    .input {
+      width: 200px;
+    }
+
+    .edit {
+      font-size: 14px;
+      font-weight: bold;
+    }
+  }
+
+  .page {
+    background: #fff;
+    width: 100%;
+    height: 50px;
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    padding-right: 20px;
+  }
+}
+</style>
---
+++ src/views/Group/components/departmentTree.vue
@@ -0,0 +1,124 @@
+<template>
+  <ul class='departmentTree-wrap'>
+    <li class="con tree-search">
+      <el-input class="search" placeholder="搜索姓名、手机、邮箱">
+        <template #suffix>
+          <el-icon class="el-input__icon">
+            <search />
+          </el-icon>
+        </template>
+      </el-input>
+    </li>
+    <li class="con tree-btns">
+      <el-button :icon="Plus" size="small">新建部门</el-button>
+      <el-button :icon="User" size="small">管理部门</el-button>
+    </li>
+    <ul class="con tree-dept">
+      <li class="tree-dept-item" v-for="item in treeDate" :key="item.name">
+        <div class="label dept-name active">{{ `${item.name}(${item.num}人)` }}</div>
+        <template v-if="item?.children">
+          <div class="child-label dept-name" v-for="child in item.children" :key="child.name">{{
+              `${child.name}(${child.num}人)`
+          }}
+          </div>
+        </template>
+      </li>
+
+    </ul>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+import { Search, Plus, User } from '@element-plus/icons-vue'
+
+
+const treeDate = [
+  {
+    name: '名称1',
+    num: 20,
+    children: [
+      { name: '名称1-1', num: 11, },
+      { name: '子部门1名称', num: 12, }
+    ]
+  },
+  {
+    name: '名称2',
+    num: 10,
+    children: [
+      {
+        name: '名称2-1',
+        num: 11,
+      },
+      {
+        name: '子部门1名称测试',
+        num: 12,
+      }]
+  }]
+</script>
+
+<style lang="scss">
+.departmentTree-wrap .tree-search {
+  .search {
+    .el-input__inner {
+      font-size: 12px;
+
+    }
+  }
+}
+</style>
+<style lang='scss' scoped>
+.departmentTree-wrap {
+  padding: 20px 0;
+  background-color: #fff;
+  border-right: 1px solid #e8e8e8;
+  z-index: 2;
+
+  .con {
+    margin-bottom: 10px;
+  }
+
+  .tree-search {
+    padding: 0 14px;
+
+    .search {
+      .el-input__inner {
+        font-size: 12px;
+
+      }
+    }
+  }
+
+  .tree-btns {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 14px;
+
+    .el-button--small {
+      padding: 12px 6px;
+    }
+  }
+
+  .tree-dept {
+    padding: 0 0 0 14px;
+    .tree-dept-item {
+      .dept-name {
+        height: 32px;
+        line-height: 32px;
+        font-size: 14px;
+
+        &.active {
+          color: $mainColor;
+          border-right: 2px solid $mainColor;
+          transform: translateX(1px);
+        }
+      }
+
+      .label {}
+
+      .child-label {
+        padding-left: 20px;
+      }
+    }
+  }
+}
+</style>
