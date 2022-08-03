4b5981bfe71f5d0531e011f22869cac0d04803ab
SEN <changsen25@163.com>
2022-08-03 09:29:45 +0800

    fix:tools

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,11 +1,10 @@
 export const debounce = (fun: any, delay?: number) => {
   let timer: any = ''
   return (args: any) => {
-    let that: any = this
     let _args = args
     clearTimeout(timer)
     timer = setTimeout(function () {
-      fun.call(that, _args)
+      fun.call(this, _args)
     }, delay ?? 300)
   }
 }
