7a259790aaefa94125aa410371d3f775090f0127
leiweibo <406637644@qq.com>
2022-08-04 09:48:44 +0800

    add 邀请好友功能

--- src/views/Cohort/components/groupDeatil.vue
+++ src/views/Cohort/components/groupDeatil.vue
@@ -14,7 +14,10 @@
     <!-- 组成员 -->
     <ul class="user-list border-b">
       <li class="li-search con">
-        <p class="li-search-con">组成员<span class="li-search-con-num">{{ total }}</span>人</p>
+        <p class="li-search-con"
+          >组成员<span class="li-search-con-num">{{ total }}</span
+          >人</p
+        >
         <el-input class="li-search-inp" placeholder="搜索成员">
           <template #suffix>
             <el-icon class="el-input__icon">
@@ -27,11 +30,15 @@
         <el-button type="primary">组织架构</el-button>
       </li> -->
       <ul class="img-list con">
+        <li class="img-list-add" @click="openDialogAdd">+</li>
+        <li class="img-list-del">-</li>
         <li class="img-list-con" v-for="item in userList" :key="item.id">
           <img class="img-list-con-img" src="@/assets/img/x.png" alt="" />
           <span class="img-list-con-name">{{ item.name }}</span>
         </li>
-        <span v-show="total>12" class="img-list-more-btn" @click="handleViewMoreUser">查看更多</span>
+        <span v-show="total > 12" class="img-list-more-btn" @click="handleViewMoreUser"
+          >查看更多</span
+        >
       </ul>
     </ul>
     <li class="con setting-con border-b">
@@ -53,60 +60,112 @@
       <el-button type="danger">解散该群</el-button>
     </div>
   </div>
+
+  <el-dialog v-model="dialogVisible" title="邀请好友" width="30%">
+    <div class="invitateBox">
+      <div
+        class="invitateBox-box"
+        v-for="(item, index) in state.friendsData"
+        :key="item.id"
+        @click="onClickBox(item, index)"
+      >
+        <div class="invitateBox-flex">
+          <img src="https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg" alt="" />
+          <div class="invitateBox-name">{{ item.name }}</div>
+        </div>
+        <div
+          class="invitateBox-btn"
+          :style="state.ids.includes(item.id) ? 'background:#466DFF' : ''"
+        >
+          <div class="invitateBox-btn-in" v-if="state.ids.includes(item.id)"></div>
+        </div>
+      </div>
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="submitInvite">确认</el-button>
+      </span>
+    </template>
+  </el-dialog>
 </template>
 
 <script lang="ts" setup>
-import API from '@/services'
-import { reactive, ref, onMounted, toRefs, watch } from 'vue'
-type infoType = {
-  detail: teamType
-  userList: userType[]
-  total: number
-}
-type prop = {
-  id: string
-  info: infoType
-}
-const { id, info } = defineProps<prop>()
-const { userList, total, detail } = toRefs(info)
+  import API from '@/services'
+  import { reactive, ref, onMounted, toRefs, watch } from 'vue'
+  import $services from '@/services'
+  import { ElMessage } from 'element-plus'
+  import { useUserStore } from '@/store/user'
 
-const state = reactive({
-  userList: [], //群成员
-  total: 0, //总数
-  isEditNickName: false, //是否编辑昵称
-  isQunName: false, //是否编辑群名称
-  isIgnoreMsg: false, //是否免打扰信息
-  isStick: false //是否置顶
-})
-
-const qunName = ref<string>('')
-const nickName = ref<string>('')
-onMounted(() => {
-  id && getQunPerson(id)
-  console.log('44', id, info)
-})
-
-// 获取群成员
-const getQunPerson = async (id: string) => {
-  const { data, err } = await API.cohort.getPersons({
-    data: {
-      id,
-      offset: 0,
-      limit: 10
-    }
-  })
-  if (!err) {
-    console.log('获取群成员', data)
-    const { result = [], total = 0 } = data
-    state.userList = result
-    state.total = total
+  interface itemResult {
+    code: string
+    createTime: string
+    createUser: string
+    id: string
+    name: string
+    status: number
+    thingId: string
+    typeName: string
+    updateTime: string
+    updateUser: string
+    version: string
   }
-}
 
-const isShowMoreUser = ref<boolean>(false)
-const handleViewMoreUser = () => {
-  isShowMoreUser.value = true
-}
+  type infoType = {
+    detail: teamType
+    userList: userType[]
+    total: number
+  }
+  type prop = {
+    id: string
+    info: infoType
+  }
+  const { id, info } = defineProps<prop>()
+  const { userList, total, detail } = toRefs(info)
+  const store = useUserStore()
+  const dialogVisible = ref(false)
+
+  const state = reactive({
+    userList: [], //群成员
+    total: 0, //总数
+    isEditNickName: false, //是否编辑昵称
+    isQunName: false, //是否编辑群名称
+    isIgnoreMsg: false, //是否免打扰信息
+    isStick: false, //是否置顶
+    friendsData: [], // 我的好友
+    ids: [] // 所选择到的好友id列表
+  })
+
+  const qunName = ref<string>('')
+  const nickName = ref<string>('')
+  onMounted(() => {
+    id && getQunPerson(id)
+    console.log('44', id, info)
+  })
+
+  let current = ref(0)
+
+  // 获取群成员
+  const getQunPerson = async (id: string) => {
+    const { data, err } = await API.cohort.getPersons({
+      data: {
+        id,
+        offset: 0,
+        limit: 10
+      }
+    })
+    if (!err) {
+      console.log('获取群成员', data)
+      const { result = [], total = 0 } = data
+      state.userList = result
+      state.total = total
+    }
+  }
+
+  const isShowMoreUser = ref<boolean>(false)
+  const handleViewMoreUser = () => {
+    isShowMoreUser.value = true
+  }
 
   // const emit = defineEmits(['submitInfo'])
 
@@ -115,146 +174,271 @@ const handleViewMoreUser = () => {
 
   //   emit('submitInfo', textarea)
   // }
+
+  // 选择人员事件
+  const onClickBox = (item: itemResult, index: number) => {
+    if (state.ids.indexOf(item.id) !== -1) {
+      state.ids.splice(state.ids.indexOf(item.id), 1)
+    } else {
+      state.ids.push(item.id)
+    }
+  }
+  // 确认邀请人员入群
+  const submitInvite = () => {
+    $services.cohort
+      .pullPerson({
+        data: {
+          id: info.detail.id,
+          targetIds: state.ids
+        }
+      })
+      .then((res: ResultType) => {
+        if (res.code == 200) {
+          ElMessage({
+            message: '邀请成功',
+            type: 'warning'
+          })
+          dialogVisible.value = false
+        } else {
+          ElMessage({
+            message: res.msg,
+            type: 'warning'
+          })
+        }
+      })
+  }
+
+  const openDialogAdd = () => {
+    dialogVisible.value = true
+    $services.person
+      .getFriends({
+        data: {
+          offset: current.value,
+          limit: 10
+        }
+      })
+      .then((res: ResultType) => {
+        if (res.code == 200) {
+          state.friendsData = res.data.result
+        } else {
+          ElMessage({
+            message: res.msg,
+            type: 'warning'
+          })
+        }
+      })
+  }
 </script>
 
 <style lang="scss" scoped>
-.group-detail-wrap {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  width: 340px;
-  border-left: 1px solid #ccc;
-  padding: 15px;
-
-  .title {
-    font-size: 18px;
-    font-weight: 600;
-    margin-bottom: 10px;
-  }
-
-  // 群基本信息
-  .base {
+  .img-list-add {
+    cursor: pointer;
+    width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border-radius: 2px;
+    color: rgba(0, 0, 0, 0.45);
+    border: 1px dashed #bfbfbf;
+    font-size: 40px;
+    display: flex;
+    justify-content: center;
     align-items: center;
-    padding-bottom: 15px;
-
-    &-img {
-      width: 60px;
-      height: 60px;
-      margin-right: 15px;
-      border-radius: 50%;
-    }
-
-    &-info {
-      display: flex;
-      justify-content: space-around;
-      flex-direction: column;
-      height: 100%;
-
-      &-top {
-        align-items: center;
-
-        &-name {
-          font-size: 16px;
-          font-weight: 600;
-          margin-right: 10px;
-        }
-      }
-
-      &-desc {
-        font-size: 13px;
-      }
-    }
+    margin-right: 16px;
   }
-
-  .con {
-    padding: 10px 0 0 0;
+  .img-list-del {
+    width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border-radius: 2px;
+    border: 1px solid #ea4c43;
+    font-size: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #ea4c43;
+    margin-right: 16px;
   }
-
-  // 群成员
-  .user-list {
-    .li-search {
+  .img-list-del {
+  }
+  .invitateBox {
+    width: 100%;
+    height: 500px;
+    overflow: auto;
+    &-flex {
       display: flex;
       justify-content: space-between;
       align-items: center;
+      img {
+        width: 60px;
+        height: 60px;
+        background-size: 100% 100%;
+        border-radius: 50%;
+      }
+    }
+    &-name {
+      margin-left: 20px;
+    }
+    &-box {
+      height: 60px;
+      display: flex;
+      align-items: center;
+      margin-bottom: 20px;
+      justify-content: space-between;
+      cursor: pointer;
+    }
+    &-btn {
+      width: 16px;
+      height: 16px;
+      background: #f3f4f9;
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      &-in {
+        width: 6px;
+        height: 6px;
+        background: #fff;
+        border-radius: 50%;
+      }
+    }
+  }
+  .group-detail-wrap {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 340px;
+    border-left: 1px solid #ccc;
+    padding: 15px;
+
+    .title {
+      font-size: 18px;
+      font-weight: 600;
+      margin-bottom: 10px;
+    }
+
+    // 群基本信息
+    .base {
+      align-items: center;
+      padding-bottom: 15px;
+
+      &-img {
+        width: 60px;
+        height: 60px;
+        margin-right: 15px;
+        border-radius: 50%;
+      }
+
+      &-info {
+        display: flex;
+        justify-content: space-around;
+        flex-direction: column;
+        height: 100%;
+
+        &-top {
+          align-items: center;
+
+          &-name {
+            font-size: 16px;
+            font-weight: 600;
+            margin-right: 10px;
+          }
+        }
+
+        &-desc {
+          font-size: 13px;
+        }
+      }
+    }
+
+    .con {
+      padding: 10px 0 0 0;
+    }
+
+    // 群成员
+    .user-list {
+      .li-search {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        &-con {
+          font-size: 15px;
+          font-weight: 500;
+          color: #333;
+
+          &-num {
+            margin-left: 10px;
+          }
+        }
+
+        &-inp {
+          width: 120px;
+        }
+      }
+    }
+
+    .img-list {
+      display: flex;
+      flex-wrap: wrap;
+      width: 100%;
 
       &-con {
-        font-size: 15px;
-        font-weight: 500;
-        color: #333;
+        display: flex;
+        flex-direction: column;
+        align-self: center;
+        margin-right: 10px;
+        margin-bottom: 10px;
+        cursor: pointer;
 
-        &-num {
-          margin-left: 10px;
+        &-img {
+          width: 50px;
+          height: 50px;
+        }
+
+        &-name {
+          font-size: 10px;
+          line-height: 20px;
+          text-align: center;
         }
       }
 
-      &-inp {
-        width: 120px;
-      }
-    }
-  }
-
-  .img-list {
-    display: flex;
-    flex-wrap: wrap;
-    width: 100%;
-
-    &-con {
-      display: flex;
-      flex-direction: column;
-      align-self: center;
-      margin-right: 10px;
-      margin-bottom: 10px;
-      cursor: pointer;
-
-      &-img {
-        width: 50px;
-        height: 50px;
-      }
-
-      &-name {
-        font-size: 10px;
-        line-height: 20px;
+      &-more-btn {
+        width: 100%;
         text-align: center;
+        color: $mainColor;
+        cursor: pointer;
       }
     }
 
-    &-more-btn {
-      width: 100%;
-      text-align: center;
-      color: $mainColor;
-      cursor: pointer;
+    .setting-con {
+      display: flex;
+      justify-content: space-between;
+
+      .con-label {
+        font-weight: bold;
+        color: #333;
+      }
+    }
+
+    .check-con {
+      .el-checkbox {
+        height: 26px;
+      }
+    }
+
+    // 底部按钮
+    .footer {
+      position: absolute;
+      bottom: 20px;
+      left: 0;
+      right: 0;
+      display: flex;
+      justify-content: center;
+    }
+
+    .border-b {
+      border-bottom: 1px solid #d6d6d6;
+      padding-bottom: 10px;
     }
   }
-
-  .setting-con {
-    display: flex;
-    justify-content: space-between;
-
-    .con-label {
-      font-weight: bold;
-      color: #333;
-    }
-  }
-
-  .check-con {
-    .el-checkbox {
-      height: 26px;
-    }
-  }
-
-  // 底部按钮
-  .footer {
-    position: absolute;
-    bottom: 20px;
-    left: 0;
-    right: 0;
-    display: flex;
-    justify-content: center;
-  }
-
-  .border-b {
-    border-bottom: 1px solid #d6d6d6;
-    padding-bottom: 10px;
-  }
-}
 </style>
--- src/views/Cohort/index.vue
+++ src/views/Cohort/index.vue
@@ -9,11 +9,15 @@
     <!-- 右侧展示主体 -->
     <div class="chart-page">
       <!-- 头部 -->
-      <GroupHeaderVue :info="selectInfo" v-if="activeInfo?.id" @viewDetail="handleViewDetail"
-        @addUserOrCohort="handleAddFun" />
+      <GroupHeaderVue :info="selectInfo" v-if="activeInfo?.id" @viewDetail="handleViewDetail" />
       <!-- 聊天区域 -->
-      <GroupContent class="chart-content" :myId="myId" ref="contentWrapRef" :list="showMsgList"
-        @viewMoreMsg="handleViewMoreHistory" />
+      <GroupContent
+        class="chart-content"
+        :myId="myId"
+        ref="contentWrapRef"
+        :list="showMsgList"
+        @viewMoreMsg="handleViewMoreHistory"
+      />
       <!-- 输入区域 -->
       <GroupInputBox class="chart-input" v-show="activeInfo?.id" @submitInfo="submit" />
     </div>
@@ -22,218 +26,209 @@
 </template>
 
 <script lang="ts" setup>
-import { useUserStore } from '@/store/user'
-import * as signalR from '@microsoft/signalr'
-import API from '@/services'
-import { onMounted, reactive, ref, Ref, watch, computed } from 'vue'
-import GroupSideBarVue from './components/groupSideBar.vue'
-import GroupHeaderVue from './components/groupHeader.vue'
-import GroupInputBox from './components/groupInputBox.vue'
-import GroupContent from './components/groupContent.vue'
-import GroupDetail from './components/groupDeatil.vue'
-interface infoType {
-  detail: teamType
-  userList: userType[]
-  total: number
-}
-const { setUserNameMap, userToken } = useUserStore()
-// 是否展示导航
-const isShowMenu = ref<boolean>(false)
-const isShowDetail = ref<boolean>(false)
-// 消息信息列表
-const msgMap = ref(new Map())
-// 是否展示红点
-const msgDotMap = ref(new Map())
-
-// 记录历史记录上次搜索信息
-const lastQueryParams = ref<any>({})
-
-const showMsgList = computed(() => {
-  return msgMap.value.get(activeInfo.value.id) ?? []
-})
-const contentWrapRef = ref(null)
-
-//获取 登录人id-用于区分信息源
-const myId = useUserStore().queryInfo.id;
-const current = ref<number>(0) //当前页码
-const limit = ref<number>(20) //数量限制 limit
-const pageOffset = computed(() => {
-  return current.value * limit.value
-})
-
-
-// 记录所选聊天对象---群或者人
-const activeInfo = ref<any>({})
-// 所选聊天对象的基本信息
-let selectInfo = reactive<infoType>({ detail: {} as teamType, userList: [], total: 0 })
-// 消息服务
-const connection = new signalR.HubConnectionBuilder()
-  .withUrl('/orginone/orgchat/msghub')
-  .withAutomaticReconnect()
-  .build()
-
-// 接受信息--处理信息
-connection.on("RecvMsg", (res) => {
-  const { data } = res
-  let fromId = data.fromId
-  if (data.toId != myId) {
-    fromId = data.toId
+  import { useUserStore } from '@/store/user'
+  import * as signalR from '@microsoft/signalr'
+  import API from '@/services'
+  import { onMounted, reactive, ref, Ref, watch, computed } from 'vue'
+  import GroupSideBarVue from './components/groupSideBar.vue'
+  import GroupHeaderVue from './components/groupHeader.vue'
+  import GroupInputBox from './components/groupInputBox.vue'
+  import GroupContent from './components/groupContent.vue'
+  import GroupDetail from './components/groupDeatil.vue'
+  interface infoType {
+    detail: teamType
+    userList: userType[]
+    total: number
   }
-  const oldMsg = msgMap.value.get(fromId) ?? []
-  msgMap.value.set(fromId, [...oldMsg, data])
-  // 信息来源是正在聊天的人 -则不展示红点
-  msgDotMap.value.set(fromId, activeInfo.value.id === fromId)
-});
+  const { setUserNameMap, userToken } = useUserStore()
+  // 是否展示导航
+  const isShowMenu = ref<boolean>(false)
+  const isShowDetail = ref<boolean>(false)
+  // 消息信息列表
+  const msgMap = ref(new Map())
+  // 是否展示红点
+  const msgDotMap = ref(new Map())
 
-onMounted(() => {
-  isShowMenu.value = true
-  connection.start().then(async () => {
-    let res = await connection.invoke('TokenAuth', userToken)
-    console.log('链接成功', res)
+  // 记录历史记录上次搜索信息
+  const lastQueryParams = ref<any>({})
+
+  const showMsgList = computed(() => {
+    return msgMap.value.get(activeInfo.value.id) ?? []
   })
-})
+  const contentWrapRef = ref(null)
 
-// 监听所选聊天对象
-watch(
-  () => activeInfo.value,
-  (val) => {
-    const { typeName, id, team = {} } = val
-    current.value = 0
-    // 取消红点提示
-    msgDotMap.value.set(id, false)
-    if (typeName === '人员') {
-      selectInfo.detail = val
-      selectInfo.total = 0
-      selectInfo.userList = []
-    } else if (typeName === '群组') {
-      selectInfo.detail = team
-      getQunPerson(id)
-    }
-
-    //获取历史记录
-    const canQueryHistory = msgMap.value.get(id)?.length ?? 0
-    if (canQueryHistory === 0) {
-      getHistoryMsg(id, typeName)
-    }
-  }
-)
-
-// 展示详情页
-const handleViewDetail = () => {
-  isShowDetail.value = !isShowDetail.value
-}
-
-// 获取群成员
-const getQunPerson = async (id: string) => {
-  const { data, success } = await API.cohort.getPersons({
-    data: {
-      id,
-      offset: 0,
-      limit: 12
-    }
+  //获取 登录人id-用于区分信息源
+  const myId = useUserStore().queryInfo.id
+  const current = ref<number>(0) //当前页码
+  const limit = ref<number>(20) //数量限制 limit
+  const pageOffset = computed(() => {
+    return current.value * limit.value
   })
-  if (success === true) {
-    const { total = 0, result = [] } = data
-    selectInfo.total = total
-    selectInfo.userList = result
-    result.forEach((item: userType) => {
-      setUserNameMap(item.id, item.name)
-    });
-  }
-}
-// 提交信息
-const submit = async (value: Ref<string>) => {
-  let toId = activeInfo.value.id
-  if (toId > 0) {
-    let res = await connection.invoke('SendMsg', {
-      toId: activeInfo.value.id,
-      msgType: 'text',
-      msgBody: value.value
+
+  // 记录所选聊天对象---群或者人
+  const activeInfo = ref<any>({})
+  // 所选聊天对象的基本信息
+  let selectInfo = reactive<infoType>({ detail: {} as teamType, userList: [], total: 0 })
+  // 消息服务
+  const connection = new signalR.HubConnectionBuilder()
+    .withUrl('/orginone/orgchat/msghub')
+    .withAutomaticReconnect()
+    .build()
+
+  // 接受信息--处理信息
+  connection.on('RecvMsg', (res) => {
+    const { data } = res
+    let fromId = data.fromId
+    if (data.toId != myId) {
+      fromId = data.toId
+    }
+    const oldMsg = msgMap.value.get(fromId) ?? []
+    msgMap.value.set(fromId, [...oldMsg, data])
+    // 信息来源是正在聊天的人 -则不展示红点
+    msgDotMap.value.set(fromId, activeInfo.value.id === fromId)
+  })
+
+  onMounted(() => {
+    isShowMenu.value = true
+    connection.start().then(async () => {
+      let res = await connection.invoke('TokenAuth', userToken)
+      console.log('链接成功', res)
     })
-    if (res.success === true) {
-      const oldMsg = msgMap.value.get(toId) ?? []
-      msgMap.value.set(toId, [...oldMsg, res.data])
-    }
-    contentWrapRef.value?.goPageEnd()
-  }
-}
-
-// 获取历史消息
-const getHistoryMsg = async (id: string, type: string) => {
-
-  const url: string = type == '人员' ? "getFriendMsg" : type == '群组' ? 'getCohortMsg' : '';
-  if (!url) {
-    return console.log('该聊天对象-非人员/群组');
-  }
-
-  const params = {
-    [type == '人员' ? "friendId" : 'cohortId']: id,
-    offset: pageOffset.value,
-    limit: limit.value
-  }
-  const { data, success } = await API.history[url]({
-    data: params
   })
-  if (success === true) {
-    const { total = 0, result = [] } = data
-    const newHistoryMsgArr = result.reverse()
-    const oldMsg = msgMap.value.get(id) ?? []
-    msgMap.value.set(id, [...newHistoryMsgArr, ...oldMsg])
-    // 记录查询成功的数据
-    lastQueryParams.value = params
-    contentWrapRef.value.keepScrollPos()
+
+  // 监听所选聊天对象
+  watch(
+    () => activeInfo.value,
+    (val) => {
+      const { typeName, id, team = {} } = val
+      current.value = 0
+      // 取消红点提示
+      msgDotMap.value.set(id, false)
+      if (typeName === '人员') {
+        selectInfo.detail = val
+        selectInfo.total = 0
+        selectInfo.userList = []
+      } else if (typeName === '群组') {
+        selectInfo.detail = team
+        getQunPerson(id)
+      }
+
+      //获取历史记录
+      const canQueryHistory = msgMap.value.get(id)?.length ?? 0
+      if (canQueryHistory === 0) {
+        getHistoryMsg(id, typeName)
+      }
+    }
+  )
+
+  // 展示详情页
+  const handleViewDetail = () => {
+    isShowDetail.value = !isShowDetail.value
   }
-}
 
-// 获取更多历史消息
-const handleViewMoreHistory = () => {
-  current.value++
-  getHistoryMsg(activeInfo.value.id, activeInfo.value.typeName)
-}
+  // 获取群成员
+  const getQunPerson = async (id: string) => {
+    const { data, success } = await API.cohort.getPersons({
+      data: {
+        id,
+        offset: 0,
+        limit: 12
+      }
+    })
+    if (success === true) {
+      const { total = 0, result = [] } = data
+      selectInfo.total = total
+      selectInfo.userList = result
+      result.forEach((item: userType) => {
+        setUserNameMap(item.id, item.name)
+      })
+    }
+  }
+  // 提交信息
+  const submit = async (value: Ref<string>) => {
+    let toId = activeInfo.value.id
+    if (toId > 0) {
+      let res = await connection.invoke('SendMsg', {
+        toId: activeInfo.value.id,
+        msgType: 'text',
+        msgBody: value.value
+      })
+      if (res.success === true) {
+        const oldMsg = msgMap.value.get(toId) ?? []
+        msgMap.value.set(toId, [...oldMsg, res.data])
+      }
+      contentWrapRef.value?.goPageEnd()
+    }
+  }
 
-//添加成员/群
-const handleAddFun = () => {
-  console.log('添加成员/群')
-}
+  // 获取历史消息
+  const getHistoryMsg = async (id: string, type: string) => {
+    const url: string = type == '人员' ? 'getFriendMsg' : type == '群组' ? 'getCohortMsg' : ''
+    if (!url) {
+      return console.log('该聊天对象-非人员/群组')
+    }
+
+    const params = {
+      [type == '人员' ? 'friendId' : 'cohortId']: id,
+      offset: pageOffset.value,
+      limit: limit.value
+    }
+    const { data, success } = await API.history[url]({
+      data: params
+    })
+    if (success === true) {
+      const { total = 0, result = [] } = data
+      const newHistoryMsgArr = result.reverse()
+      const oldMsg = msgMap.value.get(id) ?? []
+      msgMap.value.set(id, [...newHistoryMsgArr, ...oldMsg])
+      // 记录查询成功的数据
+      lastQueryParams.value = params
+      contentWrapRef.value.keepScrollPos()
+    }
+  }
+
+  // 获取更多历史消息
+  const handleViewMoreHistory = () => {
+    current.value++
+    getHistoryMsg(activeInfo.value.id, activeInfo.value.typeName)
+  }
 </script>
 
 <style lang="scss">
-.custom-group-silder-menu.el-aside {
-  height: 100%;
-}
-
+  .custom-group-silder-menu.el-aside {
+    height: 100%;
+  }
 </style>
 <style lang="scss" scoped>
-.cohort-wrap {
-  width: 100%;
-  height: calc(100vh - 60px);
-  display: flex;
-  justify-content: space-between;
-  background-color: #fff;
-
-  .chart-page {
-    height: 100%;
+  .cohort-wrap {
+    width: 100%;
+    height: calc(100vh - 60px);
     display: flex;
-    flex-direction: column;
     justify-content: space-between;
-    flex: 1;
-    overflow: hidden;
+    background-color: #fff;
 
-    .chart-content {
-      flex-grow: 1;
-      overflow-y: auto;
-    }
+    .chart-page {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      flex: 1;
+      overflow: hidden;
 
-    .chart-input {
-      height: max-content;
-      min-height: 120px;
-      border-top: 1px solid #ccc;
-
-      .el-textarea__inner {
-        color: #fff;
+      .chart-content {
+        flex-grow: 1;
+        overflow-y: auto;
       }
 
+      .chart-input {
+        height: max-content;
+        min-height: 120px;
+        border-top: 1px solid #ccc;
+
+        .el-textarea__inner {
+          color: #fff;
+        }
+      }
     }
   }
-}
 </style>
