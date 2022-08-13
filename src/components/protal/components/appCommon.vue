8d7229e0889ce6926293abd0d490ec32a87b4708
leiweibo <406637644@qq.com>
2022-08-13 10:57:11 +0800

    update:组件超出宽度问题

--- src/components/protal/components/appCommon.vue
+++ src/components/protal/components/appCommon.vue
@@ -19,86 +19,87 @@
 </template>
 
 <script lang="ts" setup>
-import { reactive } from 'vue'
-const emit = defineEmits(['joinFriend'])
-const dataList = reactive([
-  {
-    name: '添加好友',
-    introduce: '添加好友',
-    metohd: 'friend'
-  },
-  {
-    name: '资产监管资产监管',
-    introduce: '简单、⾼效、开放的监管⼯具'
-  },
-  {
-    name: '资产监管资产监管',
-    introduce: '简单、⾼效、开放的监管⼯具'
-  },
-  {
-    name: '资产监管资产监管',
-    introduce: '简单、⾼效、开放的监管⼯具'
-  },
-  {
-    name: '资产监管资产监管',
-    introduce: '简单、⾼效、开放的监管⼯具'
-  },
-  {
-    name: '资产监管资产监管',
-    introduce: '简单、⾼效、开放的监管⼯具'
+  import { reactive } from 'vue'
+  const emit = defineEmits(['joinFriend'])
+  const dataList = reactive([
+    {
+      name: '添加好友',
+      introduce: '添加好友',
+      metohd: 'friend'
+    },
+    {
+      name: '资产监管资产监管',
+      introduce: '简单、⾼效、开放的监管⼯具'
+    },
+    {
+      name: '资产监管资产监管',
+      introduce: '简单、⾼效、开放的监管⼯具'
+    },
+    {
+      name: '资产监管资产监管',
+      introduce: '简单、⾼效、开放的监管⼯具'
+    },
+    {
+      name: '资产监管资产监管',
+      introduce: '简单、⾼效、开放的监管⼯具'
+    },
+    {
+      name: '资产监管资产监管',
+      introduce: '简单、⾼效、开放的监管⼯具'
+    }
+  ])
+  const joinFriends = (item: any) => {
+    if (item.metohd == 'friend') {
+      emit('joinFriend')
+    }
   }
-])
-const joinFriends = (item: any) => {
-  if (item.metohd == 'friend') {
-    emit('joinFriend')
-  }
-}
 </script>
 
 <style lang="scss" scoped>
-.appCommon {
-  width: 100%;
-  height: 100%;
-  background: #fff;
-  display: flex;
-  flex-direction: column;
-  &-title {
-    font-size: 18px;
-    border-bottom: 1px solid rgb(232, 232, 232);
-    font-weight: 600;
-    padding: 20px;
-    margin-bottom: 10px;
-  }
-  &-content {
-    display: flex;
+  .appCommon {
     width: 100%;
-    flex: 1;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    &-box {
-      cursor: pointer;
-      width: 30%;
-      height: 45%;
-      border: 1px solid rgb(233, 233, 233);
+    height: 100%;
+    background: #fff;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    &-title {
+      font-size: 18px;
+      border-bottom: 1px solid rgb(232, 232, 232);
+      font-weight: 600;
+      padding: 20px;
+      margin-bottom: 10px;
+    }
+    &-content {
       display: flex;
-      align-items: center;
-      padding: 0 10px 0 20px;
-      img {
-        width: 60px;
-        height: 60px;
+      width: 100%;
+      flex: 1;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      &-box {
+        cursor: pointer;
+        width: 30%;
+        height: 45%;
+        border: 1px solid rgb(233, 233, 233);
+        display: flex;
+        align-items: center;
+        padding: 0 10px 0 20px;
+        img {
+          width: 60px;
+          height: 60px;
+        }
       }
     }
   }
-}
-.textBox {
-  margin-left: 20px;
-  &-top {
-    font-size: 16px;
-    font-weight: bold;
-    margin-bottom: 10px;
+  .textBox {
+    margin-left: 20px;
+    &-top {
+      font-size: 16px;
+      font-weight: bold;
+      margin-bottom: 10px;
+    }
+    &-btm {
+      font-size: 14px;
+    }
   }
-  &-btm {
-    font-size: 14px;
-  }
-}
 </style>
