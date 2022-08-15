fcac3423be66a9a5da13c020700a6036eb07edf8
leiweibo <406637644@qq.com>
2022-08-15 14:18:16 +0800

    update:自定义首页默认展示页

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,7 +4,6 @@
       <el-aside class="custom-group-silder-menu" width="220px"><Menu></Menu></el-aside>
     </teleport> -->
     <div class="headers">
-
       <Head></Head>
     </div>
     <div class="articleTop">
@@ -29,9 +28,21 @@
       </div>
     </div>
     <el-dialog v-model="dialogVisible" title="添加好友" width="30%">
-      <el-select v-model="value" filterable remote reserve-keyword placeholder="请输入要查找的好友名"
-        :remote-method="remoteMethod" :loading="loading">
-        <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+      <el-select
+        v-model="value"
+        filterable
+        remote
+        reserve-keyword
+        placeholder="请输入要查找的好友名"
+        :remote-method="remoteMethod"
+        :loading="loading"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
       </el-select>
       <template #footer>
         <span class="dialog-footer">
@@ -44,54 +55,79 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive, onMounted } from 'vue'
-import Menu from '@/views/Layout/components/menu.vue'
-import Head from './components/head.vue'
-import Invitate from './components/invitate.vue'
-import Organization from './components/organization.vue'
-import AppMarket from './components/appMarket.vue'
-import AppCommon from './components/appCommon.vue'
-import ManageSystem from './components/manageSystem.vue'
-import $services from '@/services'
-import { ElMessage } from 'element-plus'
+  import { ref, reactive, onMounted } from 'vue'
+  import Menu from '@/views/Layout/components/menu.vue'
+  import Head from './components/head.vue'
+  import Invitate from './components/invitate.vue'
+  import Organization from './components/organization.vue'
+  import AppMarket from './components/appMarket.vue'
+  import AppCommon from './components/appCommon.vue'
+  import ManageSystem from './components/manageSystem.vue'
+  import $services from '@/services'
+  import { ElMessage } from 'element-plus'
 
-interface ListItem {
-  value: string
-  label: string
-}
-const isShowMenu = ref<boolean>(false)
-const options = ref<ListItem[]>([])
-const value = ref('')
-const loading = ref(false)
-onMounted(() => {
-  isShowMenu.value = true
-})
-const remoteMethod = (query: string) => {
-  if (query) {
-    loading.value = true
+  interface ListItem {
+    value: string
+    label: string
+  }
+  const isShowMenu = ref<boolean>(false)
+  const options = ref<ListItem[]>([])
+  const value = ref('')
+  const loading = ref(false)
+  onMounted(() => {
+    isShowMenu.value = true
+  })
+  const remoteMethod = (query: string) => {
+    if (query) {
+      loading.value = true
+      $services.person
+        .searchPersons({
+          data: {
+            text: query,
+            offset: 0,
+            limit: 10
+          }
+        })
+        .then((res: ResultType) => {
+          if (res.code == 200) {
+            console.log(res)
+            let states = res.data.result
+            let arr: { value: any; label: any }[] = []
+            states.forEach((el: any) => {
+              let obj = {
+                value: el.id,
+                label: el.name
+              }
+              arr.push(obj)
+            })
+            console.log('====', options.value)
+            options.value = arr
+            loading.value = false
+          } else {
+            ElMessage({
+              message: res.msg,
+              type: 'warning'
+            })
+          }
+        })
+    } else {
+      options.value = []
+    }
+  }
+  const submitFriends = () => {
     $services.person
-      .searchPersons({
+      .applyJoin({
         data: {
-          text: query,
-          offset: 0,
-          limit: 10
+          id: value.value
         }
       })
       .then((res: ResultType) => {
         if (res.code == 200) {
-          console.log(res)
-          let states = res.data.result
-          let arr: { value: any; label: any }[] = []
-          states.forEach((el: any) => {
-            let obj = {
-              value: el.id,
-              label: el.name
-            }
-            arr.push(obj)
+          ElMessage({
+            message: '申请成功',
+            type: 'warning'
           })
-          console.log('====', options.value)
-          options.value = arr
-          loading.value = false
+          dialogVisible.value = false
         } else {
           ElMessage({
             message: res.msg,
@@ -99,101 +135,75 @@ const remoteMethod = (query: string) => {
           })
         }
       })
-  } else {
-    options.value = []
   }
-}
-const submitFriends = () => {
-  $services.person
-    .applyJoin({
-      data: {
-        id: value.value
-      }
-    })
-    .then((res: ResultType) => {
-      if (res.code == 200) {
-        ElMessage({
-          message: '申请成功',
-          type: 'warning'
-        })
-        dialogVisible.value = false
-      } else {
-        ElMessage({
-          message: res.msg,
-          type: 'warning'
-        })
-      }
-    })
-}
 
-const dialogVisible = ref(false)
-const joinFriend = () => {
-  dialogVisible.value = true
-}
-
+  const dialogVisible = ref(false)
+  const joinFriend = () => {
+    dialogVisible.value = true
+  }
 </script>
 
 <style lang="scss" scoped>
-:deep(.el-select) {
-  width: 100%;
-}
-
-.baseLayout {
-  height: 100%;
-}
-
-.headers {
-  width: 100%;
-  height: 15%;
-}
-
-.articleTop {
-  display: flex;
-  width: 100%;
-  height: 20%;
-  margin-top: 20px;
-
-  &-left {
-    width: 50%;
-    height: 100%;
-    margin-right: 20px;
-  }
-
-  &-right {
-    width: 50%;
-    height: 100%;
-  }
-}
-
-.articleBtm {
-  width: 100%;
-  height: 60%;
-  margin-top: 20px;
-  display: flex;
-
-  &-leftTop {
+  :deep(.el-select) {
     width: 100%;
-    height: 60%;
-    margin-bottom: 20px;
   }
 
-  &-leftBtm {
-    flex: 1;
+  .baseLayout {
+    height: 100%;
+  }
+
+  .headers {
+    width: 100%;
+    height: 15%;
+  }
+
+  .articleTop {
+    display: flex;
     width: 100%;
     height: 20%;
+    margin-top: 20px;
+
+    &-left {
+      width: 50%;
+      height: 100%;
+      margin-right: 20px;
+    }
+
+    &-right {
+      width: 50%;
+      height: 100%;
+    }
   }
 
-  &-box {
+  .articleBtm {
+    width: 100%;
+    height: 60%;
+    margin-top: 20px;
     display: flex;
-    flex-direction: column;
-    width: 60%;
-    height: 100%;
-    margin-right: 20px;
-  }
 
-  &-right {
-    width: 40%;
-    height: 100%;
+    &-leftTop {
+      width: 100%;
+      height: 60%;
+      margin-bottom: 20px;
+    }
+
+    &-leftBtm {
+      flex: 1;
+      width: 100%;
+      height: 20%;
+    }
+
+    &-box {
+      display: flex;
+      flex-direction: column;
+      width: 60%;
+      height: 100%;
+      margin-right: 20px;
+    }
+
+    &-right {
+      width: 40%;
+      height: 100%;
+    }
   }
-}
 </style>
