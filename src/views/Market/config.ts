c4292771d6ebd0497b95b106d1b36dfb188d9bcd
SEN <changsen25@163.com>
2022-08-23 18:03:25 +0800

    feat:市场应用展示

--- src/views/Market/config.ts
+++ src/views/Market/config.ts
@@ -1,73 +1,73 @@
 export const baseData = [
   {
-    name: '测试name1',
-    desc: '测试desc1',
+    name: '资产监管',
+    remark: '测试remark1',
     url: '测试url1',
     id: '1'
   },
   {
-    name: '测试name2',
-    desc: '测试desc2',
+    name: '资产监管',
+    remark: '测试remark2',
     url: '测试url2',
     id: '2'
   },
   {
-    name: '测试name3',
-    desc: '测试desc3',
+    name: '资产监管',
+    remark: '测试remark3',
     url: '测试url4',
     id: '3'
   },
   {
-    name: '菜市场',
-    desc: '手打',
+    name: '资产监管',
+    remark: '手打',
     url: '测试打算url1',
     id: '4'
   },
   {
-    name: '大',
-    desc: '订单',
+    name: '资产监管',
+    remark: '订单',
     url: '测发放l2',
     id: '5'
   },
   {
-    name: '会更好',
-    desc: '发发发发发发沙发发发放散阀',
+    name: '资产监管',
+    remark: '发发发发发发沙发发发放散阀',
     url: '测试url4',
     id: '6'
   },
   {
-    name: '测试name1',
-    desc: '测试desc1',
+    name: '资产监管',
+    remark: '测试remark1',
     url: '测试url1',
     id: '7'
   },
   {
-    name: '测试name2',
-    desc: '测试desc2',
+    name: '资产监管',
+    remark: '测试remark2',
     url: '测试url2',
     id: '8'
   },
   {
-    name: '测试name3',
-    desc: '测试desc3',
+    name: '资产监管',
+    remark: '测试remark3',
     url: '测试url4',
     id: '9'
   },
   {
-    name: '测试name1',
-    desc: '测试desc1',
+    name: '资产监管',
+    remark: '测试remark1',
     url: '测试url1',
     id: '10'
   },
   {
-    name: '测试name2',
-    desc: '测试desc2',
+    name: '资产监管',
+    remark: '测试remark2',
     url: '测试url2',
     id: '11'
   }
   // {
   //   name: '测试name3',
-  //   desc: '测试desc3',
+  //   remark: '测试remark3',
   //   url: '测试url4',
   //   id: '21'
   // }
@@ -76,26 +76,22 @@ export const baseData = [
 // 我的应用操作列表
 export const actionOptionsOfOwn: Array<OptionType> = [
   {
-    label: '删除',
-    value: 'delete'
-  },
-  {
     label: '上架',
-    value: 'delete'
+    value: 'putaway'
   },
   {
     label: '分享',
-    value: 'delete'
+    value: 'share'
   }
 ]
 // 其他应用操作列表
 export const actionOptionsOfOther = [
   {
     label: '退订',
-    value: 'delete'
+    value: 'unsubscribe'
   },
   {
     label: '分享',
-    value: 'delete'
+    value: 'share'
   }
 ]
