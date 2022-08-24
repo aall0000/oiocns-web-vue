c764e07293c3b1ff3515097e4e009afbad0167d8
P_uerile <1075073213@qq.com>
2022-08-24 15:01:54 +0800

    update:市场跳转

--- src/views/Market/index.vue
+++ src/views/Market/index.vue
@@ -9,7 +9,7 @@
       <ul class="box-ul">
         <p class="box-ul-title">我的应用</p>
         <li class="app-card">
-          <MarketCreate :info="add" @myclick="addApp" />
+          <MarketCreate :info="add" @myclick="GoPage('/market/markList')" />
           <ShopCard v-for="item in state.myAppList" :info="item" :key="item.id" :over-id="item.id">
             <!-- <template> -->
             <el-dropdown @command="(value) => handleCommand('own', value, item)" placement="top">
