9ca8e8ab6aeefb3f4b32bb0f1af5d34ac8a59631
jiangyang <1140938202@qq.com>
2022-08-25 14:24:59 +0800

    update:修改页面

--- src/services/RESTFULURL.ts
+++ src/services/RESTFULURL.ts
@@ -48,7 +48,7 @@ export default {
     getAuthIdentity: cohort + '/get/authority/identitys', //查询职权身份
     getAuthority: cohort + '/get/authoritys', //查询职权
     getIdentityPerson: cohort + '/get/identity/persons', //查询身份的人
-    getIdentities: cohort + '/get/identitys', //查询身份
+    getIdentitys: cohort + '/get/identitys', //查询身份
     getJoinedCohorts: cohort + '/get/joined/cohorts', //查询我加入的群
     getPersons: cohort + '/get/persons', //查询群成员
     getSubAuthorities: cohort + '/get/subauthoritys', //查询子职权
