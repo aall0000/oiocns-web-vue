10da51f6ef46c565700893b77c667c107af617dd
leiweibo <406637644@qq.com>
2022-08-30 17:54:33 +0800

    update

--- src/views/Market/AppShare/unit.vue
+++ src/views/Market/AppShare/unit.vue
@@ -1,53 +1,36 @@
 <template>
-  <div class="card" ref="cardHeight">
-    <!-- <div class="header">
-      <div class="title">{{ props.selectItem.label }}</div>
-      <div class="box-btns">
-        <div v-if="props.selectItem?.data?.typeName == '集团'">
-          <el-button small link type="primary" @click="pullCompanysDialog = true"
-            >添加单位</el-button
-          >
-        </div>
-        <div
-          v-if="
-            props.selectItem?.data?.typeName == '子集团' ||
-            props.selectItem?.data?.typeName == '工作组'
-          "
+  <div class="unitLayout">
+    <DiyTable
+      ref="diyTable"
+      :hasTableHead="true"
+      :tableData="companies"
+      @handleUpdate="handleUpdate"
+      :tableHead="tableHead"
+    >
+      <template #operate="scope">
+        <el-button
+          link
+          type="danger"
+          size="small"
+          :disabled="scope.row.id == selectItem.data?.belongId"
+          @click="removeFrom(scope.row)"
+          >移除单位</el-button
         >
-          <el-button small link type="primary" @click="showAssignDialog">分配单位</el-button>
-        </div>
-      </div>
-    </div>
-
-    <div :style="{ height: tabHeight - 30 + 'px' }">
+      </template>
+    </DiyTable>
+  </div>
+  <!-- <div :style="{ height: tabHeight - 30 + 'px' }">
       <div style="width: 100%; height: 100%">
-        <DiyTable
-          ref="diyTable"
-          :hasTableHead="true"
-          :tableData="companies"
-          @handleUpdate="handleUpdate"
-          :tableHead="tableHead"
-        >
-          <template #operate="scope">
-            <el-button
-              link
-              type="danger"
-              size="small"
-              :disabled="scope.row.id == selectItem.data?.belongId"
-              @click="removeFrom(scope.row)"
-              >移除单位</el-button
-            >
-          </template>
-        </DiyTable>
+        
       </div>
     </div> -->
-  </div>
 </template>
 
 <script setup lang="ts">
-  import { onMounted } from 'vue'
+  import { onMounted, ref } from 'vue'
   import { useRoute } from 'vue-router'
   const route = useRoute()
+  const diyTable = ref(null)
   onMounted(() => {
     console.log(route.query.id)
     getShareHistory()
@@ -56,31 +39,9 @@
 </script>
 
 <style lang="scss" scoped>
-  .card {
-    height: 100%;
+  .unitLayout {
     width: 100%;
-    background-color: #fff;
-    padding: 10px;
-
-    .header {
-      display: flex;
-
-      .title {
-        text-align: left;
-        font-size: 16px;
-        width: 30%;
-        font-weight: bold;
-      }
-      .box-btns {
-        text-align: right;
-        padding-right: 14px;
-        padding-bottom: 10px;
-        width: 70%;
-      }
-    }
-
-    .search {
-      width: 50%;
-    }
+    height: 100%;
+    background: #fff;
   }
 </style>
