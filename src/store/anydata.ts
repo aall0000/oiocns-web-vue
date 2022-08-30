975ea700542eff04aeded28a9eeccf28c5a26cca
cclu <lkrnchocolateaaron@gmail.com>
2022-08-30 11:36:41 +0800

    消息问题修复

--- src/store/anydata.ts
+++ src/store/anydata.ts
@@ -60,10 +60,10 @@ export const useAnyData = defineStore({
         updateUserSpace(params: { workspaceId: string, content: UserSpace[] }) {
             console.log('更新用户组件', params.content)
             anyStore.set(`${params.workspaceId}.user.content`,
-            {
-                operation: 'replaceAll',
-                data: params.content
-            })
+                {
+                    operation: 'replaceAll',
+                    data: params.content
+                })
             this.workspace.user = this.workspace.user || {}
             this.workspace.user.content = params.content
         },
@@ -71,10 +71,10 @@ export const useAnyData = defineStore({
         updateHomeSpace(params: { workspaceId: string, content: UsingSpace[] }) {
             console.log('更新工作空间配置', params.content)
             anyStore.set(`${params.workspaceId}.content`,
-            {
-                operation: 'replaceAll',
-                data: params.content || []
-            })
+                {
+                    operation: 'replaceAll',
+                    data: params.content || []
+                })
             this.workspace.content = params.content
         },
         // 设置个人空间全部数据
@@ -82,18 +82,27 @@ export const useAnyData = defineStore({
             this.workspace = data
         },
         // 更新已读未读消息数量
-        updateMessageNoread({groupid,userid,count}: {groupid:string,userid: string, count: number}) {
-            const newCount = (this.messageNoReadMap[userid] || 0) + count
+        updateMessageNoread({ groupid, userid, count }: { groupid: string, userid: string, count: number }) {
+
+            const newCount = (this.messageNoReadMap[groupid] && this.messageNoReadMap[groupid][userid] || 0) + count
             anyStore.set(`message.noread.${groupid}.${userid}`,
-            {
-                operation: 'replaceAll',
-                data: newCount
-            })
-            this.messageNoReadMap[userid] = newCount
+                {
+                    operation: 'replaceAll',
+                    data: newCount
+                })
+
+            if (!this.messageNoReadMap[groupid]) {
+                this.messageNoReadMap[groupid] = {}
+            }
+
+            this.messageNoReadMap[groupid][userid] = newCount
         },
-        clearMessageNodread(userid: string) {
-            anyStore.delete(`message.noread.${userid}`)
-            delete this.messageNoReadMap[userid]
+        clearMessageNodread(groupid: string, userid: string) {
+            anyStore.delete(`message.noread.${groupid}.${userid}`)
+            if (this.messageNoReadMap[groupid] && this.messageNoReadMap[groupid][userid]) {
+
+                delete this.messageNoReadMap[groupid][userid]
+            }
         },
         // 初始化未读数量
         setMessageNoRead(data: Record<string, number>) {
