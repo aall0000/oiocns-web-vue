d45eeb23c078ade3855d8a11a138f8bea8448814
SEN <changsen25@163.com>
2022-09-01 17:24:17 +0800

    fix

---
+++ src/views/Market/appRegister/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app-register-wrap">
+    <el-descriptions title="Vertical list without border" :column="4" direction="vertical">
+      <el-descriptions-item label="Username">kooriookami</el-descriptions-item>
+      <el-descriptions-item label="Telephone">18100000000</el-descriptions-item>
+      <el-descriptions-item label="Place" :span="2">Suzhou</el-descriptions-item>
+      <el-descriptions-item label="Remarks">
+        <el-tag size="small">School</el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item label="Address">
+        No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
+      </el-descriptions-item>
+    </el-descriptions>
+  </div>
+</template>
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+  .app-register-wrap {
+  }
+</style>
