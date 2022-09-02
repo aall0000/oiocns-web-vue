22021543bd4a1fa5635fb8e6d5ee05300ab3b8b0
SEN <changsen25@163.com>
2022-09-02 11:13:13 +0800

    update:颜色

---
+++ src/views/Market/AppRegister/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-register-wrap">
+    <div class="page-title"> 应用注册 </div>
+    <div class="app-base-info">
+      <p>基础信息</p>
+    </div>
+    <div class="app-base-info">
+      <p>资源信息</p>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+  .app-register-wrap {
+    height: 100%;
+    background: var(--el-bg-color-overlay);
+  }
+</style>
