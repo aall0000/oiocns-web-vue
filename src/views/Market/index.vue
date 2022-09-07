af61b9b03e3af1542a3718318cfd3cf5696c787d
P_uerile <1075073213@qq.com>
2022-09-07 11:03:39 +0800

    update:删除其他应用分享操作

--- src/views/Market/index.vue
+++ src/views/Market/index.vue
@@ -28,7 +28,14 @@
             :key="item.id"
             :over-id="item.id"
           >
-          <template #icon><HeadImg :name="item.name" :url="item.icon || appImg" :imgWidth="48" :limit="1" :isSquare="false" /></template>
+            <template #icon
+              ><HeadImg
+                :name="item.name"
+                :url="item.icon || appImg"
+                :imgWidth="48"
+                :limit="1"
+                :isSquare="false"
+            /></template>
             <template #rightIcon>
               <el-dropdown
                 trigger="click"
@@ -46,7 +53,9 @@
                       {{ action.label }}
                     </el-dropdown-item>
                     <el-dropdown-item @click="deleteApp(item)">移除应用</el-dropdown-item>
-                    <el-dropdown-item  @click="GoPage('/market/appDetail')">应用详情</el-dropdown-item>
+                    <el-dropdown-item @click="GoPage('/market/appDetail')"
+                      >应用详情</el-dropdown-item
+                    >
                   </el-dropdown-menu>
                 </template>
               </el-dropdown>
@@ -100,7 +109,14 @@
             :key="item.id"
             :over-id="item.id"
           >
-          <template #icon><HeadImg :name="item.name" :url="item.icon || appImg" :imgWidth="48" :limit="1" :isSquare="false" /></template>
+            <template #icon
+              ><HeadImg
+                :name="item.name"
+                :url="item.icon || appImg"
+                :imgWidth="48"
+                :limit="1"
+                :isSquare="false"
+            /></template>
             <template #rightIcon>
               <el-dropdown
                 trigger="click"
@@ -110,15 +126,10 @@
                 <el-icon :size="18"><Operation /></el-icon>
                 <template #dropdown>
                   <el-dropdown-menu>
-                    <el-dropdown-item
-                      v-for="action in actionOptionsOfOwn"
-                      :command="action.value"
-                      :key="action.value"
-                    >
-                      {{ action.label }}
-                    </el-dropdown-item>
                     <el-dropdown-item @click="deleteApp(item)">移除应用</el-dropdown-item>
-                    <el-dropdown-item  @click="GoPage('/market/appDetail')">应用详情</el-dropdown-item>
+                    <el-dropdown-item @click="GoPage('/market/appDetail')"
+                      >应用详情</el-dropdown-item
+                    >
                   </el-dropdown-menu>
                 </template>
               </el-dropdown>
