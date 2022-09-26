eea0ad3cc478cba0f349a23cea8806578f7841d0
realVeer <realVeer@qkbyte.com>
2022-09-26 12:32:58 +0800

    暂存

--- src/hubs/orgchat.ts
+++ src/hubs/orgchat.ts
@@ -5,6 +5,23 @@ import anyStore from '@/utils/anystore'
 import notify from '@/utils/notify'
 const hisMsgCollName = "chat-message"
 
+enum TargetType {
+    Person = "人员",
+    Company = "单位",
+    Group = "集团",
+    SubGroup = "子集团",
+    Department = "部门",
+    Job = "工作组",
+    Cohort = "群组"
+}
+
+enum MessageType {
+    Text = "文本",
+    Image = "图片",
+    Video = "视频",
+    Voice = "语音",
+    Recall = "撤回"
+}
 
 // 消息服务
 // 创建链接
