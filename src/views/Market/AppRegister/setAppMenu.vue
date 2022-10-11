0fc6d2343a823ae4f34071927f640d365ee8e26c
leiweibo <406637644@qq.com>
2022-10-11 16:54:49 +0800

    update:应用注册增加流程字段

--- src/views/Market/AppRegister/setAppMenu.vue
+++ src/views/Market/AppRegister/setAppMenu.vue
@@ -3,7 +3,7 @@
     <ul
       class="setAppMenu-wrap"
       :class="className"
-      v-for="(menuItem, index) in menus"
+      v-for="(menuItem, menuIndex) in menus"
       :key="menuItem.customId"
       @click.stop
     >
@@ -48,11 +48,186 @@
         <div class="menu-label">资源编码:</div>
         <el-input placeholder="请设置" v-model="menuItem.code" :readonly="readOnly"></el-input>
       </li>
-      <el-divider v-if="index !== menus.length - 1" />
+
+      <div class="custom-title">
+        <p> <span class="custom-span"></span> 流程信息 </p>
+        <el-icon class="add-btn" :size="20" @click.stop="handleAddMenu('Add', menuIndex)">
+          <CirclePlus />
+        </el-icon>
+      </div>
+      <ul
+        class="setToDoMenu-wrap"
+        :class="className"
+        v-for="(item, fIndex) in menuItem.flows"
+        :key="item.customId"
+        @click.stop
+      >
+        <li class="menu-item flex">
+          <div :class="['menu-label', readOnly ? '' : 'required']">业务信息:</div>
+          <el-input
+            placeholder="请设置流程名称"
+            v-model="item.name"
+            :readonly="readOnly"
+          ></el-input>
+          <el-icon
+            class="child-btn"
+            v-if="!readOnly"
+            @click.stop="handleAddMenu('Delete', menuIndex, item.customId)"
+            ><Delete
+          /></el-icon>
+          <el-icon
+            class="child-btn"
+            v-if="!readOnly"
+            @click.stop="handleAddMenu('Up', menuIndex, item.customId)"
+            ><SortUp
+          /></el-icon>
+          <el-icon
+            class="child-btn"
+            v-if="!readOnly"
+            @click.stop="handleAddMenu('Down', menuIndex, item.customId)"
+            ><SortDown
+          /></el-icon>
+        </li>
+        <!-- 共有部分 -->
+        <div class="custom-title">
+          <p> <span class="custom-span"></span> 流程字段 </p>
+          <el-icon
+            class="add-btn"
+            :size="20"
+            @click.stop="handlFieldMenu('Add', menuIndex, fIndex)"
+          >
+            <CirclePlus />
+          </el-icon>
+        </div>
+        <ul
+          class="setToDoMenu-wrap"
+          :class="className"
+          v-for="(fItem, index) in item.field"
+          :key="item.customId"
+          @click.stop
+        >
+          <li class="menu-item flex">
+            <div :class="['menu-label', readOnly ? '' : 'required']">字段名称:</div>
+            <el-input
+              placeholder="请设置字段名称"
+              v-model="fItem.key"
+              :readonly="readOnly"
+            ></el-input>
+            <el-icon
+              class="child-btn"
+              v-if="!readOnly"
+              @click.stop="handlFieldMenu('Delete', menuIndex, fIndex, fItem.customId)"
+              ><Delete
+            /></el-icon>
+            <el-icon
+              class="child-btn"
+              v-if="!readOnly"
+              @click.stop="handlFieldMenu('Up', menuIndex, fIndex, fItem.customId)"
+              ><SortUp
+            /></el-icon>
+            <el-icon
+              class="child-btn"
+              v-if="!readOnly"
+              @click.stop="handlFieldMenu('Down', menuIndex, fIndex, item.customId)"
+              ><SortDown
+            /></el-icon>
+          </li>
+          <li class="menu-item flex">
+            <div :class="['menu-label', readOnly ? '' : 'required']">字段编号:</div>
+            <el-input
+              placeholder="请设置字段编号"
+              v-model="fItem.code"
+              :readonly="readOnly"
+            ></el-input>
+          </li>
+          <li class="menu-item flex">
+            <div :class="['menu-label', readOnly ? '' : 'required']">字段类型:</div>
+            <el-select v-model="fItem.type" class="m-2" placeholder="Select">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select>
+            <!-- <el-input
+              placeholder="请设置字段编号"
+              v-model="fItem.code"
+              :readonly="readOnly"
+            ></el-input> -->
+          </li>
+        </ul>
+      </ul>
+      <div class="custom-title">
+        <p> <span class="custom-span"></span> 应用组件 </p>
+        <el-icon class="add-btn" :size="20" @click.stop="handleAddComponents('Add', menuIndex)">
+          <CirclePlus />
+        </el-icon>
+      </div>
+      <ul
+        class="setToDoMenu-wrap"
+        :class="className"
+        v-for="(comItem, index) in menuItem.components"
+        :key="comItem.customId"
+        @click.stop
+      >
+        <li class="menu-item flex">
+          <div :class="['menu-label', readOnly ? '' : 'required']">组件名称:</div>
+          <el-input
+            placeholder="请设置流程名称"
+            v-model="comItem.name"
+            :readonly="readOnly"
+          ></el-input>
+          <el-icon
+            class="child-btn"
+            v-if="!readOnly"
+            @click.stop="handleAddComponents('Delete', menuIndex, comItem.customId)"
+            ><Delete
+          /></el-icon>
+          <el-icon
+            class="child-btn"
+            v-if="!readOnly"
+            @click.stop="handleAddComponents('Up', menuIndex, comItem.customId)"
+            ><SortUp
+          /></el-icon>
+          <el-icon
+            class="child-btn"
+            v-if="!readOnly"
+            @click.stop="handleAddComponents('Down', menuIndex, comItem.customId)"
+            ><SortDown
+          /></el-icon>
+        </li>
+        <!-- 共有部分 -->
+        <li class="menu-item flex">
+          <div :class="['menu-label', readOnly ? '' : 'required']">链接地址:</div>
+          <el-input
+            placeholder="请设置链接地址"
+            v-model="comItem.url"
+            :readonly="readOnly"
+          ></el-input>
+        </li>
+        <li class="menu-item flex">
+          <div :class="['menu-label', readOnly ? '' : 'required']">组件宽度:</div>
+          <el-input
+            placeholder="请设置宽度"
+            v-model="comItem.width"
+            :readonly="readOnly"
+          ></el-input>
+        </li>
+        <li class="menu-item flex">
+          <div :class="['menu-label', readOnly ? '' : 'required']">组件高度:</div>
+          <el-input
+            placeholder="请设置高度"
+            v-model="comItem.height"
+            :readonly="readOnly"
+          ></el-input>
+        </li>
+      </ul>
+      <el-divider v-if="menuIndex !== menus.length - 1" />
     </ul>
   </template>
   <template v-else>
-    <div class="noData" style="text-align: center;margin-top:50px">暂无资源</div>
+    <div class="noData" style="text-align: center; margin-top: 50px">暂无资源</div>
   </template>
 </template>
 <script lang="ts" setup>
@@ -63,18 +238,76 @@
     className?: string
     isChildren?: boolean
   }
+
+  const options = [
+    {
+      value: 'NUMERIC',
+      label: '数字'
+    },
+    {
+      value: 'STRING',
+      label: '字符串'
+    }
+  ]
+
   // type ProductMenuEventType = 'add' | 'delete' | 'up' | 'down'
   const { menus, className, readOnly } = withDefaults(defineProps<Props>(), {
     isChildren: false,
     readOnly: false
   })
-  const emit = defineEmits(['handleMemuEvent'])
-  const handleEvent = (type: ProductMenuEventType, id: string) => {
+  const emit = defineEmits([
+    'handleMemuEvent',
+    'handleAddMenu',
+    'handleAddComponents',
+    'handlFieldMenu'
+  ])
+  const handleEvent = (type: ProductMenuEventType, id: string | number) => {
     emit('handleMemuEvent', type, id)
+  }
+  const handlFieldMenu = (
+    type: ProductMenuEventType,
+    index: number,
+    fIndex: number,
+    selectId?: string
+  ) => {
+    emit('handlFieldMenu', type, index, fIndex, selectId)
+  }
+  const handleAddComponents = (type: ProductMenuEventType, index: number, selectId?: string) => {
+    emit('handleAddComponents', type, index, selectId)
+  }
+  const handleAddMenu = (type: ProductMenuEventType, index: number, selectId?: string) => {
+    emit('handleAddMenu', type, index, selectId)
   }
 </script>
 
 <style lang="scss" scoped>
+  // 自定义标题
+  .custom-title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 26px;
+    font-size: 14px;
+    margin-left: 40px;
+    margin-bottom: 10px;
+
+    .custom-span {
+      display: inline-block;
+      width: 3px;
+      height: 14px;
+      margin-right: 6px;
+      background-color: #3e5ed8;
+    }
+  }
+  .add-btn {
+    cursor: pointer;
+    color: var(--el-color-primary);
+    margin: 0 10px;
+  }
+  .setToDoMenu-wrap {
+    width: calc(100% - 40px);
+    margin-left: 40px;
+  }
   .setAppMenu-wrap {
     min-height: 100px;
 
