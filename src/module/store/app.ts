fd84616bd7c520c339f35f3ad80c8bdc1c6e86bb
jiangyang <1140938202@qq.com>
2022-10-11 17:13:40 +0800

    add:添加发起流程步骤

--- src/module/store/app.ts
+++ src/module/store/app.ts
@@ -135,6 +135,28 @@ class appStore {
     return data
   }
 
+     /**
+   *@desc 获取应用资源
+   * @param appid 应用id 
+   * @return 返回应用资源
+   */
+   public async getResource(appid:string) {
+    const { data, success } = await API.product.searchResource({
+      data: {
+        id: appid,
+        offset: 0,
+        limit: 1000,
+        filter: ''
+      }
+    })
+    if (!success) {
+      return
+    }
+    const { result = [], total = 0 } = data
+    let tabs = result
+    return tabs
+}
+
   /**
    * @desc 修改应用信息
    * @param params 应用信息
